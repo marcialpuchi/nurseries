b6145847fd5a980a9721abc2a375262e1ea26452
Marcial Puchi <marcialpuchi@gmail.com>
2018-03-19 17:39:32 -0600

    Add dashboard page

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import LandingPage from './components/pages/LandingPage';
 import LoginPage from './components/pages/LoginPage';
+import DashboardPage from './components/pages/DashboardPage';
 
 const App = () => (
-	<div>
-		<Route path='/' exact component={LandingPage}/>
-		<Route path='/login' exact component={LoginPage}/>
-	</div>
+  <div>
+    <Route path="/" exact component={LandingPage} />
+    <Route path="/login" exact component={LoginPage} />
+    <Route path="/dashboard" exact component={DashboardPage} />
+  </div>
 );
 
 export default App;
